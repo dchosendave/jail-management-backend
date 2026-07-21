2fef384f26fbe51beaa4a87cd8414eaa538dafc9
Lowie Dave Dichoson <lowiedave30@gmail.com>
2026-07-22 04:38:34 +0800

    feat: get all users gand get specific users

--- src/users/users.handlers.ts
+++ src/users/users.handlers.ts
@@ -3,7 +3,8 @@ import { db } from '../db/connection.js';
 import * as z from 'zod';
 import { UsersSchema } from './users.schemas.js';
 import bcrypt from 'bcrypt';
-import { users } from '../../drizzle/schema.js';
+import { personnel, users } from '../../drizzle/schema.js';
+import { eq } from 'drizzle-orm/sql';
 
 export async function createUser(req: Request, res: Response) {
     const body = req.body as z.infer<typeof UsersSchema>;
@@ -20,7 +21,6 @@ export async function createUser(req: Request, res: Response) {
 
     const passwordHash = await bcrypt.hash(body.password, 10);
 
-
     const [result] = await db.insert(users).values({
         personnelId: body.personnelId,
         email: body.email,
@@ -30,15 +30,57 @@ export async function createUser(req: Request, res: Response) {
         createdBy: 1,
         updatedAt: null,
         updatedBy: null
-    } as typeof users.$inferInsert).returning({ insertedId: users.userId});
+    } as typeof users.$inferInsert).returning({ email: users.email });
 
-    return res.status(201).send({ message: 'Resource created.', newId: result });
+    return res.status(201).send({ message: `Welcome to ${result?.email}` });
 }
 
 export async function getUser(req: Request, res: Response) {
+    const userId = Number(req.params.id);
 
+    const result = await db.select({
+        userId: users.userId,
+        personnelId: personnel.personnelId,
+        firstName: personnel.firstName,
+        middleName: personnel.middleName,
+        lastName: personnel.lastName,
+        suffix: personnel.suffix,
+        rank: personnel.rank,
+        designation: personnel.designation,
+        badgeNumber: personnel.badgeNumber,
+        email: users.email,
+        contactNumber: personnel.contactNumber,
+        dateOfBirth: personnel.dateOfBirth,
+        dateHired: personnel.dateHired,
+    }).from(users).innerJoin(personnel, eq(users.personnelId, personnel.personnelId)).where(eq(users.userId, userId));
+
+    if (result.length === 0) {
+        return res.status(404).send({ message: 'resource not found. ' });
+    }
+
+    return res.status(201).send({ data: result });
 }
 
 export async function getUsers(req: Request, res: Response) {
+    const result = await db.select({
+        userId: users.userId,
+        personnelId: personnel.personnelId,
+        firstName: personnel.firstName,
+        middleName: personnel.middleName,
+        lastName: personnel.lastName,
+        suffix: personnel.suffix,
+        rank: personnel.rank,
+        designation: personnel.designation,
+        badgeNumber: personnel.badgeNumber,
+        email: users.email,
+        contactNumber: personnel.contactNumber,
+        dateOfBirth: personnel.dateOfBirth,
+        dateHired: personnel.dateHired,
+    }).from(users).innerJoin(personnel, eq(users.personnelId, personnel.personnelId));
 
+    if (result.length === 0) {
+        return res.status(404).send({ message: 'resource not found. ' });
+    }
+
+    return res.status(201).send({ data: result });
 }
